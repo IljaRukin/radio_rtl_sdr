2faaae3f4cdf9329f95d3a8d7f4b392306a02518
IljaRukin <toxin543@gmail.com>
2020-12-05 23:15:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# radio_rtl_sdr
+experiments with radio signal modulation and decoding
+
+## radio.py
+is a script to play radio in real time using a rtl sdr stick
+
+## modulation.ipynb
+describes the most important modulation types (amplitude,phase,frequency,qam) and has simple implementations to decode them
+
+## decode_radio.py
+has step by step plots how the radio decoding can be done
+
+## decode_gsm.py
+trying to decode gsm mobile signals. work in progress!
---
+++ radio.py
@@ -0,0 +1,40 @@
+import asyncio
+from rtlsdr import RtlSdr
+from scipy.signal import decimate
+import numpy as np
+import sounddevice as sd
+
+async def streaming():
+	freq = 89.8e6 #mhz
+	sample_rate = 285000 #285000,1140000
+	bandwidth = 180e+3
+
+	sdr = RtlSdr()
+	sdr.sample_rate = sample_rate
+	sdr.center_freq = freq
+	sdr.bandwidth = bandwidth
+	sdr.freq_correction = 60 #PPM
+	sdr.gain = 'auto'
+
+	volume = 0.1#0.02 #multiplier
+	sound_decimate = int(sample_rate/44100)
+	sound_sample_rate = sample_rate/sound_decimate
+	audio = sd.OutputStream(channels=1,samplerate=sound_sample_rate)
+	audio.start()
+
+	async for samples in sdr.stream():
+		freq = np.real(samples)[1:]*np.diff(np.imag(samples)) - np.imag(samples)[1:]*np.diff(np.real(samples))
+		freq = decimate(freq,sound_decimate)
+		freq = volume*freq
+		#sd.play(freq,sound_sample_rate)
+		#print(audio.write_available())
+		audio.write(freq.astype('float32'))
+
+	# to stop streaming:
+	await sdr.stop()
+
+	# done
+	sdr.close()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(streaming())
