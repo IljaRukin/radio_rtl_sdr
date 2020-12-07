0df6576ec512e8615b6e2e4bf0ff47d96fc94d21
IljaRukin <ilja.rukin@gmail.com>
2020-12-07 02:49:46 +0100

    first commit

--- radio.py
+++ radio.py
@@ -6,17 +6,17 @@ import sounddevice as sd
 
 async def streaming():
 	freq = 89.8e6 #mhz
-	sample_rate = 285000 #285000,1140000
+	sample_rate = 285000 #285000,1140000,2280000
 	bandwidth = 180e+3
 
 	sdr = RtlSdr()
 	sdr.sample_rate = sample_rate
 	sdr.center_freq = freq
 	sdr.bandwidth = bandwidth
-	sdr.freq_correction = 60 #PPM
+	sdr.freq_correction = 50 #PPM
 	sdr.gain = 'auto'
 
-	volume = 0.1#0.02 #multiplier
+	volume = 0.02 #multiplier
 	sound_decimate = int(sample_rate/44100)
 	sound_sample_rate = sample_rate/sound_decimate
 	audio = sd.OutputStream(channels=1,samplerate=sound_sample_rate)
@@ -26,8 +26,7 @@ async def streaming():
 		freq = np.real(samples)[1:]*np.diff(np.imag(samples)) - np.imag(samples)[1:]*np.diff(np.real(samples))
 		freq = decimate(freq,sound_decimate)
 		freq = volume*freq
-		#sd.play(freq,sound_sample_rate)
-		#print(audio.write_available())
+		#sd.play(freq,sound_sample_rate) #slow api for playback
 		audio.write(freq.astype('float32'))
 
 	# to stop streaming:
